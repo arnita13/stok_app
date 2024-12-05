02fd841a8fa778858426627c997730e12b7a7456
arnita13 <almaidaha@gmail.com>
2024-12-05 03:01:42 +0000

    pertemuan 25

--- resources/views/stok/stok.blade.php
+++ resources/views/stok/stok.blade.php
@@ -76,7 +76,7 @@
                                 </tr>
                             </thead>
                             <tbody>
-                                {{-- @foreach ($getData as $item)
+                                @foreach ($getData as $item)
                                     <tr style="background-color: #434343">
                                         <td class="text-center">
                                             {{ (($getData->currentPage() - 1) * $getData->perPage()) + $loop->iteration }} 
@@ -86,7 +86,7 @@
                                                 {{ $item->kode_barang }} 
                                             </p>
                                         </td>
-                                        <td width="550px">{{ $item->nama_barang }} </td>
+                                        <td width="450px">{{ $item->nama_barang }} </td>
                                         <td>{{ 'Rp ' . number_format($item->harga, 0, ',', '.') }} </td>
                                         <td class="text-center">{{ $item->stok }} </td>
                                         <td>{{ $item->getSuplier->nama_suplier }} </td>
@@ -101,10 +101,10 @@
                                             </a>
                                         </td>
                                     </tr>
-                                @endforeach --}}
+                                @endforeach
                             </tbody>
                         </table>
-                        {{-- {{ $getData->links() }} --}}
+                        {{ $getData->links() }}
 
                     </div>
                 </div>
